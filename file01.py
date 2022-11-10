ae64d6b79fe360d96d6c4de05ad032e0e46bc378
turonov <turonov5400gmail.com>
2022-11-10 17:43:53 +0500

    1

--- file01.py
+++ file01.py
@@ -1,4 +1,4 @@
-def main(data:str) -> list():
+def main(data:str):
     """
     The data is from the file. Return data as a list type.
     Args:
@@ -6,10 +6,12 @@ def main(data:str) -> list():
     Returns:
         list: return answer
     """
+    b=[]
+    for i in a:
+        b.append(int(i))
     return data
 
 # Read data from file
 f = open("txt_file/data01.txt").read()
 a=f.split(",")
-b = [a]
 print(main(a))
