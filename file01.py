e8371a1239057118d90a50a75f8aa746b1a8abef
turonov <turonov5400gmail.com>
2022-11-10 16:35:54 +0500

    01

--- file01.py
+++ file01.py
@@ -6,9 +6,9 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    return data.split()
+    return data
 
 # Read data from file
-f = open("txt_file/data01.txt").read()
+f = open("txt_file/data01.txt").read().split()
 #a=[f]
 print(main(f))
